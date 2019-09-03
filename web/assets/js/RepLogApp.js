95c7bd329bad3b0a864ff13546a48cde1082c0a2
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2019-09-03 20:22:52 +0400

    master 13. The window Object & Global Variables

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,5 +1,7 @@
-(function() {
-    var RepLogApp = {
+'use strict';
+
+(function(window, $) {
+    window.RepLogApp = {
         initialize:              function ($wrapper) {
             this.$wrapper = $wrapper;
             Helper.initialize($wrapper);
@@ -58,4 +60,4 @@
             return totalWeight;
         },
     }
-})()
+})(window, jQuery)

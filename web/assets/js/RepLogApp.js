5c33fba6e552acd26ca92a9666a4b690c341381b
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2019-09-04 21:22:34 +0400

    master 22. POSTing to the API Endpoint

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -43,10 +43,15 @@
         handleNewFormSubmit: function (e) {
             e.preventDefault();
             var $form = $(e.currentTarget);
+            var formData = {};
+            $.each($form.serializeArray(), function (key, fieldData) {
+                formData[fieldData.name] = fieldData.value;
+            });
+
             $.ajax({
                 url: $form.data('url'),
                 method: 'POST',
-                data: $form.serialize(),
+                data: JSON.stringify(formData),
                 success: function(data) {
                     // todo
                     console.log('success!');

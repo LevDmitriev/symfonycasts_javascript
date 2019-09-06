b1aab2a6dd1464a5d0532e90c754ae57ba9b50d0
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2019-09-06 15:46:39 +0400

    master 28. Catching a Failed Promise

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -18,11 +18,10 @@
             var self = this;
             $.ajax({
                 url: Routing.generate('rep_log_list'),
-                success: function (data) {
-                    $.each(data.items, function (key, repLog) {
-                        self._addRow(repLog);
-                    })
-                }
+            }).then(function (data) {
+                $.each(data.items, function (key, repLog) {
+                    self._addRow(repLog);
+                })
             })
         },
         updateTotalWeightLifted: function () {
@@ -44,12 +43,11 @@
             $.ajax({
                 url:     deleteUrl,
                 method:  'DELETE',
-                success: function () {
-                    $row.fadeOut('normal', function () {
-                        $(this).remove();
-                        self.updateTotalWeightLifted();
-                    });
-                }
+            }).then(function () {
+                $row.fadeOut('normal', function () {
+                    $(this).remove();
+                    self.updateTotalWeightLifted();
+                });
             });
         },
         handleRowClick:          function () {
@@ -64,29 +62,23 @@
             $.each($form.serializeArray(), function (key, fieldData) {
                 formData[fieldData.name] = fieldData.value;
             });
-            $.ajax({
-                url: $form.data('url'),
-                method: 'POST',
-                data: JSON.stringify(formData),
-                success: function(data) {
+            this._saveRepLog(formData)
+                .then(function (data) {
                     self._clearForm();
                     self._addRow(data);
-                },
-                error: function (jqXHR) {
-                    var errorData = JSON.parse(jqXHR.responseText);
-                    self._mapErrorsToForm(errorData.errors);
-                }
-            }).then(function (data) {
-                console.log('I am successful!');
-                console.log(data);
-
-                return data;
-            }).then(function (data) {
-                console.log('Another handler');
-                console.log(data);
-            })
+                }).catch(function (jqXHR) {
+                var errorData = JSON.parse(jqXHR.responseText);
+                self._mapErrorsToForm(errorData.errors);
+            });
         },
 
+        _saveRepLog: function(data) {
+            return $.ajax({
+                url: Routing.generate('rep_log_new'),
+                method: 'POST',
+                data: JSON.stringify(data),
+            })
+        },
         _mapErrorsToForm: function (errorData) {
             // reset things
             var $form = this.$wrapper.find(this._selectors.newRepForm);
